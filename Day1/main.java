4adfb7ea34f151510255b12bea42c568e31ea0f6
vignesh-mvgs <vigneshbalanmvgs2003@gmail.com>
2024-12-09 07:24:36 +0530

    start

--- Day1/main.java
+++ Day1/main.java
@@ -1,15 +1,25 @@
 public class Main {
   public static void main(String[] args) {
-    int x = 3;
+    int x = 5;
     fun1(x);
   }
 
   public static void fun1(int x) {
-    System.out.println(x); // Print the current value of x
-    if (x > 0) { // Base condition to stop recursion
-      fun1(x - 1);
+    if (x > 0) {
+      // Print stars in ascending order
+      for (int i = 0; i < x; i++) {
+        System.out.print("*");
+      }
+      System.out.println(); // Move to the next line
+      fun1(x - 1); // Recursive call with decremented x
     }
-    System.out.println(x);
-  }
 
+    // Print stars in descending order after recursion
+    if (x > 0) {
+      for (int i = x; i > 0; i--) {
+        System.out.print("*");
+      }
+      System.out.println(); // Move to the next line
+    }
+  }
 }
